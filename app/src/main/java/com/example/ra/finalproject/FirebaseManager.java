f4f46a1d7676b40e668b48958c25624eb6b28a19
Ra Kuro <raphaeloper@gmail.com>
2019-01-22 23:02:32 +0200

    Add Firebase login and sign up via email.  Includes email and password checks.  Add new class FirebaseManager, which deals with anything Firebase related. Need to add logout button.

---
+++ app/src/main/java/com/example/ra/finalproject/FirebaseManager.java
@@ -0,0 +1,52 @@
+package com.example.ra.finalproject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FirebaseManager {
+    public static final FirebaseAuth auth = FirebaseAuth.getInstance();
+    ;
+    private Context context;
+
+    public FirebaseManager(Context context) {
+        this.context = context;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void signUp(String email, String pass) {
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(context, "Successful sign up", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Failed to sign up", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void login(String email, String pass) {
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener((Activity) context, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(context, "Successful login", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Failed to login, try again or sign up", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/ra/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/ra/finalproject/LoginActivity.java
@@ -1,13 +1,80 @@
 package com.example.ra.finalproject;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    FirebaseManager manager;
+    EditText etEmail, etPass;
+    Button btnLogin, btnSignUp;
+    private FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            } else {
+                Toast.makeText(LoginActivity.this, "Logged out", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        manager = new FirebaseManager(this);
+        etEmail = (EditText) findViewById(R.id.et_email);
+        etPass = (EditText) findViewById(R.id.et_pass);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnSignUp = (Button) findViewById(R.id.btn_signup);
+
+        btnLogin.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseManager.auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null)
+            FirebaseManager.auth.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (etEmail.getText().length() != 0 && etPass.getText().length() != 0) {
+            String email = etEmail.getText().toString();
+            String pass = etPass.getText().toString();
+            if (legit(email, pass)) {
+                if (v == btnLogin)
+                    manager.login(email, pass);
+                else if (v == btnSignUp)
+                    manager.signUp(email, pass);
+            } else
+                Toast.makeText(LoginActivity.this, "Email must be valid, Password must contain 8 or more characters",
+                        Toast.LENGTH_LONG).show();
+        } else
+            Toast.makeText(LoginActivity.this, "All fields must be filled out in order to continue", Toast.LENGTH_LONG).show();
+    }
+
+    public boolean legit(String email, String pass) {
+        return email.contains("@") && email.contains(".") && pass.length() > 7;
     }
 }
